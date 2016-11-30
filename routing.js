4dd8e8ed37d9dc7a892d26fbf3d1b1e3be21d394
Dan Riches <driches@ymail.com>
2016-11-30 10:58:35 -0800

    Fixed the logic in the controller, deleted and added the directives to the html, fixed a broken h1 tag

--- routing.js
+++ routing.js
@@ -97,15 +97,12 @@ app.controller('mainController', function($scope){
          //Interest Page Controller
         this.toggleInterest = function(interest){
             console.log('interest : ',interest);
-            IHC.checkInterest('book-club');
             if(this.checkInterest(interest)){
                 var interestIndexToRemove = signUpService.aNightIn.indexOf(interest);
                 console.log('interestIndexToRemove : ',interestIndexToRemove);
                 console.log('signUpService.aNightIn : ',signUpService.aNightIn);
                 signUpService.aNightIn.splice(interestIndexToRemove,1);
-
             } else {
-
                 signUpService.aNightIn.push(interest);
             }
             console.log(' Here is the signUpService.aNightIn : ', signUpService.aNightIn);
@@ -119,8 +116,6 @@ app.controller('mainController', function($scope){
                 console.log('CheckInterest is false');
                 return false;
             }
-
-
         }
 })
     .controller('interestsOutController', function($scope){
