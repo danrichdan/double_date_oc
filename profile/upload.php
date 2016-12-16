4196c474b802d3bd8e135963df23f5b59a21a915
wadewooldridge <waderwooldridge@gmail.com>
2016-12-16 11:24:32 -0800

    Remove any login check from uploading images.

--- profile/upload.php
+++ profile/upload.php
@@ -13,44 +13,36 @@ $response = [
     'success' => false
 ];
 
-// Check that we have a session logged in; don't need to be too specific about who is logged in.
-if ($_SESSION && $_SESSION['user_record']) {
-    // Check that the required information has been supplied.
-    if (isset($_FILES) && isset($_FILES['file']) && isset($_FILES['file']['tmp_name'])) {
-        $tempFile = $_FILES['file']['tmp_name'];
-        $basePath = dirname(__FILE__) . $ds . '..' . $ds;
-        $randomName = sha1_file($tempFile);
-        $targetPathLocal = $uploadDir . $ds . $randomName;
-        $targetPathUrl = $uploadDir . '/' . $randomName;
-        $targetFile = $basePath . $targetPathLocal;
+// Picture upload happens before we log in, so don't check for a user_record in the $_SESSION.
+if (isset($_FILES) && isset($_FILES['file']) && isset($_FILES['file']['tmp_name'])) {
+    $tempFile = $_FILES['file']['tmp_name'];
+    $basePath = dirname(__FILE__) . $ds . '..' . $ds;
+    $randomName = sha1_file($tempFile);
+    $targetPathLocal = $uploadDir . $ds . $randomName;
+    $targetPathUrl = $uploadDir . '/' . $randomName;
+    $targetFile = $basePath . $targetPathLocal;
 
-        if (move_uploaded_file($tempFile, $targetFile)) {
-            // Try to resize the image to a standard 600x450 with "best fit".
-            $pic = new Imagick($targetFile);
-            $pic->resizeImage(600, 450, Imagick::FILTER_LANCZOS, 1, TRUE);
-            $pic->writeImage($targetFile);
-            $pic->destroy();
+    if (move_uploaded_file($tempFile, $targetFile)) {
+        // Try to resize the image to a standard 600x450 with "best fit".
+        $pic = new Imagick($targetFile);
+        $pic->resizeImage(600, 450, Imagick::FILTER_LANCZOS, 1, TRUE);
+        $pic->writeImage($targetFile);
+        $pic->destroy();
 
-            // Set up success status back to the user, including the new file path.
-            $response['success'] = true;
-            $response['newLink'] = $targetPathUrl;
-        } // Error moving file.
-        else {
-            dd_error_log("profile/upload move_uploaded_file failed");
-            $response['message'] = 'move_uploaded_file failed';
-        }
-
-    } // Necessary information not supplied.
+        // Set up success status back to the user, including the new file path.
+        $response['success'] = true;
+        $response['newLink'] = $targetPathUrl;
+    } // Error moving file.
     else {
-        dd_error_log("profile/upload invalid request");
-        $response['message'] = 'Invalid request';
-        $response['files'] = $_FILES;
+        dd_error_log("profile/upload move_uploaded_file failed");
+        $response['message'] = 'move_uploaded_file failed';
     }
-}
-// No user logged in.
+
+} // Necessary information not supplied.
 else {
-    dd_error_log("profile/upload not logged in");
-    $response['message'] = 'Not logged in';
+    dd_error_log("profile/upload invalid request");
+    $response['message'] = 'Invalid request';
+    $response['files'] = $_FILES;
 }
 
 // Return the response to the user.
